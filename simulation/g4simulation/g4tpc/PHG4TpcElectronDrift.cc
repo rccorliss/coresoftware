cf60661951ba3f21612f73a28112c4e1052df83e
Ross Corliss <rcorliss@mit.edu>
2021-02-02 03:20:10 -0500

    minor

--- simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.cc
+++ simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.cc
@@ -3,6 +3,7 @@
 
 #include "PHG4TpcElectronDrift.h"
 #include "PHG4TpcDistortion.h"
+#include "PHG4TpcCentralMembrane.h"
 #include "PHG4TpcPadPlane.h"  // for PHG4TpcPadPlane
 
 #include <g4main/PHG4Hit.h>
