88859c8414c33bfdd0c085d8a6328877d592728c
Ross Corliss <rcorliss@mit.edu>
2021-02-08 21:25:09 -0500

    still has a lot of print statements as I try to figure out why tpcElectronDrift is dying

--- simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.h
+++ simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.h
@@ -42,9 +42,11 @@ class PHG4TpcElectronDrift : public SubsysReco, public PHParameterInterface
   //! detector name
   void Detector(const std::string &d)
   {
-    printf("Heard %s.  Setting\n",d);
-    detector = d;
-    printf("detector=%s\n",detector);
+    printf("tried to set detector.  skipped for sake or argument\n");
+    // printf("Heard %s.  Setting\n",d);
+    //detector = d;
+    //printf("detector=%s\n",detector);
+    return;
   }
 
   //! detector name
