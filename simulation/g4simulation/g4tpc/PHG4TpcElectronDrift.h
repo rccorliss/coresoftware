bbbf3b7e6dc9a56bd430e67620960fd41035457d
Ross Corliss <rcorliss@mit.edu>
2021-06-01 03:19:28 -0400

    debug

--- simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.h
+++ simulation/g4simulation/g4tpc/PHG4TpcElectronDrift.h
@@ -80,8 +80,8 @@ class PHG4TpcElectronDrift : public SubsysReco, public PHParameterInterface
 
   
   bool do_addCmHits=true;
-  //PHG4TpcCentralMembrane * membrane;
-  PHG4HitContainer *cmHits;
+  PHG4TpcCentralMembrane * membrane;
+  //PHG4HitContainer *cmHits;
   int centralMembraneDelay; //ns
   
   
