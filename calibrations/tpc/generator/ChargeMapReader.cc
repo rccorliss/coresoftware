f48b1a8bab645f788a396f2e68c8744028651b28
Ross Corliss <rcorliss@mit.edu>
2022-01-31 11:16:48 -0500

    debugging

--- calibrations/tpc/generator/ChargeMapReader.cc
+++ calibrations/tpc/generator/ChargeMapReader.cc
@@ -171,7 +171,7 @@ void ChargeMapReader::RegenerateDensity(){
   }
   if (hSourceCharge==nullptr) {
     //the source data doesn't exist, so we will fail if we try to clone
-    printf("no source charge data file is open, or the histogram was not found.\n"});
+    printf("no source charge data file is open, or the histogram was not found.\n");
     return;
   }
 
