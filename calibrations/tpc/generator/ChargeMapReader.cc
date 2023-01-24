6e1800b7bb23ffe60e6d79fde45d8b589367a54a
Ross Corliss <rcorliss@mit.edu>
2023-01-24 00:13:39 -0500

    debug statements

--- calibrations/tpc/generator/ChargeMapReader.cc
+++ calibrations/tpc/generator/ChargeMapReader.cc
@@ -180,13 +180,13 @@ void ChargeMapReader::RegenerateCharge()
         }
 	if (1){
 	  if (CanInterpolateAt(phimid, rmid, zmid,hChargeDensity)){
-            printf("density debug report (interp) (r,phi,z)=(%.2f, %.2f,%.2f), q_dens=%E, density=%E, vol=%E, q_bin=%E, q_interp=%E q_bin/vol=%E\n",
+            printf("density debug report (interp) (r,phi,z)=(%.2f, %.2f,%.2f), q_dens=%E, density=%E, vol=%E, q_bin=%E, q_interp=%E, q_bin/vol=%E\n",
 		   rmid, phimid, zmid, q,scaleFactor,
 		   hSourceCharge->GetBinContent(hSourceCharge->FindBin(phimid, rmid, zmid)),
 		   hSourceCharge->Interpolate(phimid, rmid, zmid),
 		   hSourceCharge->GetBinContent(hSourceCharge->FindBin(phimid, rmid, zmid))/scaleFactor);
 	  } else {
-	    printf("density debug report (getbin) (r,phi,z)=(%.2f, %.2f,%.2f), q_dens=%E, density=%E, vol=%E, q_bin=%E, q_bin/vol=%E\n",
+            printf("density debug report (getbin) (r,phi,z)=(%.2f, %.2f,%.2f), q_dens=%E, density=%E, vol=%E, q_bin=%E, q_bin/vol=%E\n",
 		   rmid, phimid, zmid, q,scaleFactor,
 		   hSourceCharge->GetBinContent(hSourceCharge->FindBin(phimid, rmid, zmid)),
 		   hSourceCharge->GetBinContent(hSourceCharge->FindBin(phimid, rmid, zmid))/scaleFactor);
