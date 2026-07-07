8d86e7e24dddd555d2423d03d426d2740cc3a6ca
Ross Corliss <rcorliss@mit.edu>
2026-07-07 14:53:50 -0400

    debugging

--- offline/packages/PHGarfield/PHGarfield.cc
+++ offline/packages/PHGarfield/PHGarfield.cc
@@ -207,13 +207,13 @@ magrot.RotateZ(theta_z);
 return;
 }
 void PHGarfield::MoveTpc(double x, double y, double z){
-printf("Translating TPC by (%f,%f,%f) radians",x,y,z);
+printf("Translating TPC by (%f,%f,%f) cm\n",x,y,z);
 
 tpcpos.SetXYZ(x,y,z);
 return;
 }
 void PHGarfield::RotateTpc(double theta_x, double theta_y, double theta_z){
-  printf("Rotating TPC by (%f,%f,%f) radians",theta_x,theta_y,theta_z);
+  printf("Rotating TPC by (%f,%f,%f) radians\n",theta_x,theta_y,theta_z);
 tpcrot.RotateX(theta_x);
 tpcrot.RotateY(theta_y);
 tpcrot.RotateZ(theta_z);
@@ -221,7 +221,7 @@ return;
 }
 
 void PHGarfield::ConvertToLocal(double &x, double &y, double &z, TRotation rot, TVector3 trans) const{
-  printf("Convert to Local (%f,%f,%f)",x,y,z);
+  //printf("Convert to Local (%f,%f,%f)\n",x,y,z);
 
   //this assumes everything is in the same units!
   //convert coords from global coords in global axes
@@ -238,7 +238,7 @@ void PHGarfield::ConvertToLocal(double &x, double &y, double &z, TRotation rot, 
   return;
 }
 void PHGarfield::ConvertToGlobal(double &x, double &y, double &z, TRotation rot, TVector3 trans) const{
-  printf("Convert to Global (%f,%f,%f)",x,y,z);
+  printf("Convert to Global (%f,%f,%f)\n",x,y,z);
   //this assumes everything is in the same units!
   //inverse of the ConvertToLocal:
   TVector3 local;
@@ -495,7 +495,7 @@ double PHGarfield::bounder(double phi, double phi_min)
 
 TPolyLine3D* PHGarfield::ReverseDriftTpcCoords(double x_cm, double y_cm, double z_cm, double step_ns)
 {
-  printf("ReverseDrifting (TPC COords) (%f,%f,%f, step=%f)",x_cm,y_cm,z_cm,step_ns);
+  printf("ReverseDrifting (TPC COords) (%f,%f,%f, step=%f)\n",x_cm,y_cm,z_cm,step_ns);
 
   //x,y,z are denominated in tpc coordinate, so transform them to global
   double x=x_cm,y=y_cm, z=z_cm;
